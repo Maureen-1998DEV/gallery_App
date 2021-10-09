90f8ed91f1e1dc9698e2171e7567dcd560a5f1a1
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-10-09 17:25:31 +0300

    pushing templates

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mygallery.apps.Mygallery',
+    'mygallery.apps.MygalleryConfig',
+    
     'bootstrap5',
 ]
 MIDDLEWARE = [
