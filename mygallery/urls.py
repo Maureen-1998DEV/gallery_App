1fc1635fa0b1d8b3858e31242fed352c08ba9630
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-10-11 12:52:29 +0300

    debbugging

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.main_gallery,name = 'main_gallery'),
     url(r'^location/(\d+)',views.location,name = 'location'),
-    url(r'^search/',views.search,name='search')]
+    url(r'^search/',views.search,name='search'),
+    url(r'^image/(?P<category>\w+)/(?P<image_id>\d+)',views.single,name = 'single')
+]
--- mygallery/views.py
+++ mygallery/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
-from .models import Image,Location
+from .models import Image,Location,categories
 
 # Create your views here.
 def main_gallery(request):
@@ -8,11 +8,28 @@ def main_gallery(request):
     locations = Location.objects.all()
     return render(request, 'index.html', {"images":images,"locations":locations})
 
+
+def single(request,category_name,image_id):
+
+    title = 'Image'
+    locations = Location.objects.all()
+    # category = Category.get_category_id(id = image_category)
+    categories = Image.objects.filter(image_category__name = category_name)
+    try:
+        image = Image.objects.get(id = image_id)
+    except:
+        raise Http404()
+    return render(request,"single.html",{'title':title,"image":image, "locations":locations, "categories":categories})
+
+
+
 def location(request,location):
     locations = Location.objects.all()
     selected_location = Location.objects.get(id = location)
     images = Image.objects.filter(location = selected_location.id)
     return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
+
+
 
 def search(request):
     if 'category' in request.GET and request.GET["category"]:
