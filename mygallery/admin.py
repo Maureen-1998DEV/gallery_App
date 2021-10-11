d998205b4f18276258c24a2ee0df03e1d3cb1dc7
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-10-11 08:54:51 +0300

    modified  admin.py  file

--- mygallery/admin.py
+++ mygallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from mygallery.models import Image,categories,Location
+
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(categories)
+admin.site.register(Location)
